a9c685a42f5c4a49751f2c48b929c2d407c2c2fb
Gregor Adams <greg@pixelass.com>
2017-06-01 09:56:20 +0200

    docs(examples): update examples

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,5 @@
 /* global document */
-import {easeOutSine, easeInQuad, easeOutQuad} from 'easing-utils'
+import {easeOutSine, easeInQuad, easeOutQuad, easeOutBack} from 'easing-utils'
 import {
   belt,
   coverLeft,
@@ -85,6 +85,64 @@ coverRightCustom(ex2, {
   },
   onEnd(next, prev, current) {
     ex2Logs.forEach(log => {
+      if (log.parentNode === current) {
+        log.style.transform = ''
+      }
+    })
+  }
+})
+
+const coverCustom = (glider, opts) => wrapper(glider, {
+  ...opts,
+  onSlide({left, right}, next, prev, current) {
+    if (prev) {
+      current.style.transform = `translate3d(${(easeOutSine(right) * -100)}%,0,0)`
+      current.style.zIndex = 2
+      prev.style.transform = `translate3d(0%,0,0)`
+      prev.style.zIndex = 1
+    } else if (next) {
+      current.style.transform = ''
+      current.style.zIndex = 1
+      next.style.zIndex = 2
+      next.style.transform = `translate3d(${100 - (easeOutSine(right) * 100)}%,0,0)`
+    }
+    if (typeof opts.onSlide === 'function') {
+      opts.onSlide({left, right}, next, prev, current)
+    }
+  },
+  onEnd(next, prev, current) {
+    current.style.zIndex = ''
+    next.style.zIndex = ''
+    prev.style.zIndex = ''
+    if (typeof opts.onEnd === 'function') {
+      opts.onEnd(next, prev, current)
+    }
+  }
+})
+
+const ex3 = document.querySelector('.ex3')
+const ex3Logs = Array.from(ex3.querySelectorAll('.log') || [])
+coverCustom(ex3, {
+  classNames,
+  initialSlide: 2,
+  speed: 600,
+  spring: 300,
+  onSlide({left, right}, next, prev, current) {
+    if (next) {
+      next.style.backgroundPosition = `${easeOutBack(right) * 100}% 50%`
+    }
+    ex3Logs.forEach(log => {
+      if (log.parentNode === next) {
+        log.style.transform = `translate3d(${100 - (easeInQuad(right) * 100)}%,0,0)`
+      } else if (log.parentNode === prev) {
+        log.style.transform = `translate3d(${-100 + (easeOutQuad(left) * 100)}%,0,0)`
+      } else if (log.parentNode === current) {
+        log.style.transform = `translate3d(${(easeOutQuad(right) * -100)}%,0,0)`
+      }
+    })
+  },
+  onEnd(next, prev, current) {
+    ex3Logs.forEach(log => {
       if (log.parentNode === current) {
         log.style.transform = ''
       }
