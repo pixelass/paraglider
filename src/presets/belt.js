8aaecb5eef8a52562be8abc40d2f76a3eb70aec8
Gregor Adams <greg@pixelass.com>
2017-06-01 09:08:14 +0200

    fix(presets): direction on belt

--- src/presets/belt.js
+++ src/presets/belt.js
@@ -4,11 +4,11 @@ const belt = (glider, opts) => wrapper(glider, {
   ...opts,
   onSlide({left, right}, next, prev, current) {
     if (prev) {
-      prev.style.transform = `translate3d(${100 - (left * 100)}%,0,0)`
+      prev.style.transform = `translate3d(${-100 + (left * 100)}%,0,0)`
     } else if (next) {
-      next.style.transform = `translate3d(${-100 + (right * 100)}%,0,0)`
+      next.style.transform = `translate3d(${100 - (right * 100)}%,0,0)`
     }
-    current.style.transform = `translate3d(${(right * 100)}%,0,0)`
+    current.style.transform = `translate3d(${(right * -100)}%,0,0)`
     if (typeof opts.onSlide === 'function') {
       opts.onSlide({left, right}, next, prev, current)
     }
