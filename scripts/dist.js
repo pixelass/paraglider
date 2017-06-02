4a344093d7763db10c343c21d81125b20c4b1f5b
Gregor Adams <greg@pixelass.com>
2017-06-02 11:19:28 +0200

    build(dist): allow building a dist version

---
+++ scripts/dist.js
@@ -0,0 +1,4 @@
+import {dist} from './run-build'
+
+dist(true)
+dist()
---
+++ scripts/run-dist.js
@@ -0,0 +1,40 @@
+import {createWriteStream} from 'fs'
+import path from 'path'
+import mkdirp from 'mkdirp'
+import rm from 'rimraf'
+import log from 'winston'
+import browserify from 'browserify'
+import watchify from 'watchify'
+import errorify from 'errorify'
+
+const distFile = path.join(__dirname, '../src/dist.js')
+const distFolder = path.join(__dirname, '../dist')
+
+const dist = (minify) => {
+  const b = browserify({
+    entries: [distFile],
+    plugin: [errorify]
+  })
+  const bundle = () => {
+    b.bundle().pipe(createWriteStream(path.join(distFolder, `paraglider.${ext}`)))
+  }
+  b.on('log', message => log.info(message))
+  b.on('error', message => log.error(message))
+  if (minify) {
+    b.transform({
+      global: true
+    }, 'uglifyify')
+  }
+  const ext = minify ? 'min.js' : 'js'
+  rm(distFolder, err => {
+    if (err) {
+      throw err
+    }
+    mkdirp(distFolder, err => {
+      if (err) {
+        throw err
+      }
+        bundle()
+    })
+  })
+}
