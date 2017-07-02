48a0cef13a284eaa91d161bdf917908fb6cce3aa
Gregor Adams <greg@pixelass.com>
2017-07-02 23:59:05 +0200

    refactor(exports): plugin only exports API
    Only export API. Wrappers and presets need to be imported separately
    
    BREAKING CHANGE: No access to presets in main

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@
  * @module Paraglider
  * @author Gregor Adams <greg@pixelass.com>
  */
-
 import Glider from './glider'
-import * as presets from './presets'
 
-export {presets}
 export default Glider
