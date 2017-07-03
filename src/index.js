bd2710823b93511014e875f0040a0db2f073644b
Gregor Adams <greg@pixelass.com>
2017-07-03 02:57:33 +0200

    refactor(polyfill): import babel-polyfill

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
  * @module Paraglider
  * @author Gregor Adams <greg@pixelass.com>
  */
+import 'babel-polyfill' // eslint-disable-line import/no-unassigned-import
 import Glider from './glider'
 
 export default Glider
